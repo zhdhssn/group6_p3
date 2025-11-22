b7ac60606083527753f2a11f81b9757bf01d8199
chsiao4_ncstate <chsiao4@ncsu.edu>
2025-11-22 12:23:10 -0500

    added instr mem yaml and code

---
+++ yaml/imem_yaml_files/uvmf_template_output/verification_ip/interface_packages/imem_pkg/src/imem_typedefs_hdl.svh
@@ -0,0 +1,22 @@
+//----------------------------------------------------------------------
+// Created with uvmf_gen version 2023.4_2
+//----------------------------------------------------------------------
+// pragma uvmf custom header begin
+// pragma uvmf custom header end
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+//     
+// DESCRIPTION: 
+// This file contains defines and typedefs to be compiled for use in
+// the simulation running on the emulator when using Veloce.
+//
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+//
+                                                                               
+
+typedef bit [15:0] bit_16;
+
+// pragma uvmf custom additional begin
+// pragma uvmf custom additional end
+
